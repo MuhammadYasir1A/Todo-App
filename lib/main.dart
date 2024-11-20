fdabde65fdfab935603591e79541077d589d4708
unknown <MuhammadYasir1A>
2024-11-20 12:26:36 +0500

    sample commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey/model/task_data.dart';
+import 'screens/task_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+ 
+  // This widget is the root of your application.
+ @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<TaskData>(
+      create: (_) => TaskData(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Provider Test',
+        home: TaskScreen(),
+      ),
+    );
+  }
+}
